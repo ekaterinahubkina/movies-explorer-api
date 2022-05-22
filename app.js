b8549780e81d47bae3400e51587832d498e36f84
Kate Hubkina <ekaterinahubkina@yandex.by>
2022-05-23 02:01:18 +0300

    fix cors

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
 const auth = require('./middlewares/auth');
 const errorHandler = require('./middlewares/errorHandler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const ErrorNotFound = require('./errors/ErrorNotFound');
 
 const { NODE_ENV, DB_URL } = process.env;
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 app.use(express.json());
 
 mongoose.connect(NODE_ENV === 'production' ? DB_URL : 'mongodb://localhost:27017/moviesdb-dev');
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://movies.hubkina.nomoredomains.xyz/'],
+  credentials: true,
+}));
 
 app.use(requestLogger);
 
